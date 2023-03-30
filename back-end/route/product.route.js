7419dfb402e6c379254338805d3e8ff6fefe5721
NDJafu <phu.2174801030168@vanlanguni.vn>
2023-03-30 14:50:20 +0700

    delete current back-end before merge

--- back-end/route/product.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const ProductController = require("../controller/product.controller")
-
-router.route("/")
-    .get(ProductController.getAllProducts)
-    .post(ProductController.insertProducts)
-
-module.exports = router
